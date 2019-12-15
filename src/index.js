c770b5b1ee2e967d35025f16ac4626b5ada09d6b
Michael Lazarski <michael.lazarski@quantilope.com>
2019-12-15 20:12:58 +0200

    🛠 add dotenv and load it while starting the server and load the port from the .env file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import setupServer from './setupServer'
+import setupServer from './setupServer.js'
 
 const start = async () => {
   const server = await setupServer({
     logger: true
   })
-
-  server.listen(3000, err => {
+  server.listen(process.env.PORT, err => {
     if (err) throw err
     console.log(`Server listening at http://localhost:${server.server.address().port}`)
   })
